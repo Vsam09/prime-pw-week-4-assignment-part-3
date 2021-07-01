7a1f7fb686697236df83e40180f97fa05dd6b4f5
samson vang <vangsam2@gmail.com>
2021-07-01 09:58:26 -0500

    Cart is running properly so far

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,20 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem( item ){
+  basket.push( item );
+  return true;
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+function listItems( basket ){
+  for ( item in basket );
+  return basket;
+}
+console.log( 'Listed items in basket', listItems( basket ));
+console.log( 'Making burgers for dinner');
+addItem( basket.push('ground beef', 'burger buns', 'lettuce', 'ketchup', 'mustard', 'pickles') );
+console.log( 'Items in basket are now', listItems( basket) );
