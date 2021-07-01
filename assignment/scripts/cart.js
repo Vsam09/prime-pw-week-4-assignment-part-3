9a0b00164be22e8c99eb3798d795f81f26c74d37
samson vang <vangsam2@gmail.com>
2021-07-01 11:09:31 -0500

    Onto wk4 part3 creating function called empty

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,18 +3,25 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = [];
 function addItem( item ){
-  basket.push( item );
+  basket.push( 'ground beef', 'burger buns', 'lettuce', 'ketchup', 'mustard',
+  'pickles' );
   return true;
 }
-console.log(`Basket is ${basket}`);
+/*console.log(`Basket is ${basket}`);
 console.log('Adding apples (expect true)', addItem('apples'));
-console.log(`Basket is now ${basket}`);
+console.log(`Basket is now ${basket}`);*/
 
-function listItems( basket ){
+function listItems( item ){
   for ( item in basket );
   return basket;
 }
-console.log( 'Listed items in basket', listItems( basket ));
+console.log( `Items in basket ${basket}` );
 console.log( 'Making burgers for dinner');
-addItem( basket.push('ground beef', 'burger buns', 'lettuce', 'ketchup', 'mustard', 'pickles') );
-console.log( 'Items in basket are now', listItems( basket) );
+console.log( 'Added burger items', addItem() );
+console.log( 'Items in basket are now', listItems() );
+console.log( `First item is ${basket[ 0 ]}` );
+console.log( `Second item is ${basket[ 1 ]}` );
+console.log( `Third item is ${basket[ 2 ]}` );
+console.log( `Fourth item is ${basket[ 3 ]}` );
+console.log( `Fifth item is ${basket[ 4 ]}` );
+console.log( `Sixth item is ${basket[ 5 ]}` );
